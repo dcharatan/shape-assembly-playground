5ae2ec3f6c6180267cb23174696c28ab5161ad6c
David Charatan <david.charatan@gmail.com>
2021-02-28 18:41:14 -0500

    UI improvements.

--- src/features/naming-task/NamingInterface.jsx
+++ src/features/naming-task/NamingInterface.jsx
@@ -7,6 +7,7 @@ import NameField from './NameField';
 import SelectionBox from './SelectionBox';
 import PrecomputeButton from './PrecomputeButton';
 import { getFloatParameters } from '../executor/executorSlice';
+import Code from '../editing-task/tutorial/Code';
 
 const NamingInterface = () => {
   const dispatch = useDispatch();
@@ -64,24 +65,39 @@ const NamingInterface = () => {
     </div>
   );
   const nameArea = (
-    <div className={nameFieldActive ? 'my-2' : undefined}>
-      <SelectionBox selected={nameFieldActive} onSelect={() => setActiveItem(-1)} header={header}>
+    <div className={nameFieldActive ? 'mb-2' : undefined}>
+      <SelectionBox selected={nameFieldActive} onSelect={() => dispatch(setActiveItem(-1))} header={header} first>
         {nameFieldActive && nameField}
       </SelectionBox>
     </div>
   );
 
+  const parameterNames = Object.entries(parameterMap).map(
+    ([name], floatParameterIndex) => names[name] ?? `parameter ${floatParameterIndex + 1}`
+  );
+  const heading = (
+    <div className="border-bottom">
+      <div className="p-3">
+        <div>You are currently naming:</div>
+        <Code>{`${names.abstraction ?? 'function'}(${parameterNames.join(', ')})`}</Code>
+      </div>
+    </div>
+  );
+
   return (
     <div className="rounded border h-100 w-100 d-flex flex-column">
-      <div className="d-flex flex-grow-1 flex-column overflow-y-scroll p-2">
-        {nameArea}
-        {parameters}
+      <div className="d-flex flex-grow-1 flex-column overflow-y-scroll">
+        {heading}
+        <div className="p-2">
+          {nameArea}
+          {parameters}
+        </div>
       </div>
       <div className="border-top p-2 d-flex flex-row align-items-center">
         <Button
           size="sm"
           className="m-2"
-          // disabled={Object.entries(parameterMap).length + 1 !== Object.entries(names).length}
+          disabled={Object.entries(parameterMap).length + 1 !== Object.entries(names).length}
           onClick={() => dispatch(startNextTask())}
         >
           Submit Names
